454599d3f92f245cbbe19c24373046bc717b5993
Nikhil Jindal <nik.jindal20@gmail.com>
2018-01-05 22:32:26 +0530

    Update and rename dt_author_id.py to classification_algos/dt_author_id.py

--- classification_algos/dt_author_id.py
+++ classification_algos/dt_author_id.py
@@ -23,7 +23,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+### Decision Tree Classifier training and testing
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 clf = tree.DecisionTreeClassifier(min_samples_split = 40)
@@ -32,7 +32,6 @@ clf = clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 
 acc = accuracy_score(labels_test,pred)
-print(acc)
-print(len(features_train[0]))
+print(acc)  ###0.9732
 
 #########################################################
